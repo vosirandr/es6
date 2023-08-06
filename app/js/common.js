a1809308aa72307229494f49028740006784c6f3
Andrey <vosirandr@gmail.com>
2023-08-07 02:02:35 +0300

    edit 1

--- app/js/common.js
+++ app/js/common.js
@@ -1,18 +1,17 @@
 // for...of
 // ---------------
-
+  /* 
 const names = ['Jack', 'Max', 'Leo'];
 
-for(let name in names) {
-  //console.log(name);
-}
+// for(let name in names) {
+//   console.log(name);
+// }
 
-
-for(let index in names) {
-  //console.log(names[index]);
-}
-
+// for(let index in names) {
+//   console.log(names[index]);
+// }
 
 for(let name of names) {
   console.log(name);
 }
+ */
